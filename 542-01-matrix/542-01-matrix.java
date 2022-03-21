7a47e703e303a851da858349ecc4237d92cfe1c5
Samrat Bhardwaj <68969450+Samrat-Bhardwaj@users.noreply.github.com>
2022-03-21 22:04:10 +0530

    Time: 53 ms (9.50%), Space: 76.8 MB (18.44%) - LeetHub

---
+++ 542-01-matrix/542-01-matrix.java
@@ -0,0 +1,52 @@
+class Solution {
+    public int[][] updateMatrix(int[][] mat) {
+        int n=mat.length;
+        int m=mat[0].length;
+        
+        LinkedList<Integer> que=new LinkedList<>();
+        
+        int[][] ans=new int[n][m];
+        
+        for(int[] e:ans){
+            Arrays.fill(e,-1); // -1 -> unvisited
+        }
+        
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(mat[i][j]==0){
+                    que.addLast(i*m+j);
+                }
+            }
+        }
+        
+        int[][] dirs={{0,1},{1,0},{0,-1},{-1,0}};
+        
+        int level=0;
+        while(que.size()>0){
+            int size=que.size();
+            
+            while(size-->0){
+                int idx=que.removeFirst();
+                
+                int i=idx/m;
+                int j=idx%m;
+                
+                if(ans[i][j]!=-1) continue;
+                
+                ans[i][j]=level;
+                
+                for(int[] dir:dirs){
+                    int x=i+dir[0];
+                    int y=j+dir[1];
+                    
+                    if(x>=0 && y>=0 && x<n && y<m && ans[x][y]==-1){
+                        que.addLast(x*m+y);
+                    }
+                }
+            }
+            level++;
+        }
+        
+        return ans;
+    }
+}
