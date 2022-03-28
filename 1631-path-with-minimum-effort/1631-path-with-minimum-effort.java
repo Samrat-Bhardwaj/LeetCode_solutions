64fd4208109226a7a48ee096a6e31d00fc2ad350
Samrat Bhardwaj <68969450+Samrat-Bhardwaj@users.noreply.github.com>
2022-03-28 23:48:24 +0530

    Time: 70 ms (70.01%), Space: 58.4 MB (59.99%) - LeetHub

---
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.java
@@ -0,0 +1,51 @@
+class Solution {
+    public int minimumEffortPath(int[][] heights) {
+        int n=heights.length;
+        int m=heights[0].length;
+        
+        int[][] dis=new int[n][m];
+        
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                dis[i][j]=(int)(1e8);
+            }
+        }
+        
+        PriorityQueue<int[]> pq=new PriorityQueue<>((int[] a, int[] b)->{
+            return a[2] - b[2];
+        });
+        
+        pq.add(new int[]{0,0,0});
+        dis[0][0]=0;
+        
+        int[][] dirs={{0,1},{1,0},{0,-1},{-1,0}};
+        
+        while(pq.size()>0){
+            int[] p=pq.remove();
+            
+            int i=p[0];
+            int j=p[1];
+            int effort_till_now=p[2];
+            
+            if(dis[i][j] < effort_till_now) continue;
+            
+            for(int[] dir:dirs){
+                int x=i+dir[0];
+                int y=j+dir[1];
+                
+                if(x>=0 && y>=0 && x<n && y<m){
+                    int curr_effort=Math.abs(heights[i][j]-heights[x][y]);
+                    
+                    int final_effort=Math.max(curr_effort,effort_till_now);
+                    
+                    if(dis[x][y] > final_effort){
+                        dis[x][y]=final_effort;
+                        pq.add(new int[]{x,y,final_effort});
+                    }
+                }
+            }
+        }
+        
+        return dis[n-1][m-1];
+    }
+}
