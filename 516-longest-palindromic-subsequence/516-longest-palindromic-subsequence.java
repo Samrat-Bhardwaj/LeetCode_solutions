f9eb93cfcbd846e6a0b6aa3d5e459ad9b0f9f342
Samrat Bhardwaj <68969450+Samrat-Bhardwaj@users.noreply.github.com>
2022-04-25 21:42:00 +0530

    Time: 89 ms (41.33%), Space: 106.1 MB (5.01%) - LeetHub

--- 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.java
+++ 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.java
@@ -33,10 +33,31 @@ class Solution {
         }
     }
     
+    public int tab(String s){
+        int n=s.length();
+        
+        int[][] dp=new int[n][n];
+        
+        for(int diag=0; diag<n; diag++){
+            for(int i=0,j=diag; j<n; i++,j++){
+                if(diag==0){
+                    dp[i][j]=1;
+                } else if(s.charAt(i)==s.charAt(j)){
+                    dp[i][j]=dp[i+1][j-1]+2;
+                } else {
+                    dp[i][j]=Math.max(dp[i+1][j],dp[i][j-1]);
+                }
+            }
+        }
+        
+        return dp[0][n-1];
+    }
+    
     public int longestPalindromeSubseq(String s) {
         int n=s.length();
         int[][] memo=new int[n][n];
         // return rec(s,0,s.length()-1);
-        return rec_memo(s,0,n-1,memo);
+        // return rec_memo(s,0,n-1,memo);
+        return tab(s);
     }
 }
