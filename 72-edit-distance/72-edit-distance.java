88187e85eb7edbb7f9edc31dee8e810fc945518e
Samrat Bhardwaj <68969450+Samrat-Bhardwaj@users.noreply.github.com>
2022-04-27 00:01:29 +0530

    Time: 10 ms (30.79%), Space: 46.9 MB (5.25%) - LeetHub

--- 72-edit-distance/72-edit-distance.java
+++ 72-edit-distance/72-edit-distance.java
@@ -32,11 +32,36 @@ class Solution {
         return memo[n][m]=Math.min(insert,Math.min(delete,replace))+1;
     }
     
+    public int tab(String w1, String w2, int n, int m){
+        int[][] dp=new int[n+1][m+1];
+        
+        for(int i=0; i<=n; i++){
+            for(int j=0; j<=m; j++){
+                if(i==0){
+                    dp[i][j]=j;
+                } else if(j==0){
+                    dp[i][j]=i;
+                } else if(w1.charAt(i-1)==w2.charAt(j-1)){
+                    dp[i][j]=dp[i-1][j-1];
+                } else {
+                    int insert=dp[i][j-1];
+                    int delete=dp[i-1][j];
+                    int replace=dp[i-1][j-1];
+                    
+                    dp[i][j]=Math.min(insert,Math.min(delete,replace))+1;
+                }
+            }
+        }
+        
+        return dp[n][m];
+    }
+    
     public int minDistance(String word1, String word2) {
         int n=word1.length();
         int m=word2.length();
         
         int[][] memo=new int[n+1][m+1];
-        return rec_memo(word1,word2,n,m,memo);
+        // return rec_memo(word1,word2,n,m,memo);
+        return tab(word1,word2,n,m);
     }
 }
