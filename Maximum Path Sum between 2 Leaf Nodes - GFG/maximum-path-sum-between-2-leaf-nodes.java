2ac2b00b1ecdc1ad7237751e681db998ba0cd81d
Samrat Bhardwaj <68969450+Samrat-Bhardwaj@users.noreply.github.com>
2022-04-28 07:33:07 +0530

    Added solution - LeetHub

---
+++ Maximum Path Sum between 2 Leaf Nodes - GFG/maximum-path-sum-between-2-leaf-nodes.java
@@ -0,0 +1,155 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.util.LinkedList; 
+import java.util.Queue; 
+import java.io.*;
+import java.util.*;
+
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+
+class GFG {
+    
+    static Node buildTree(String str){
+        
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+        
+        Queue<Node> queue = new LinkedList<>(); 
+        
+        queue.add(root);
+        // Starting from the second element
+        
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+    static void printInorder(Node root){
+        if(root == null)
+            return;
+            
+        printInorder(root.left);
+        System.out.print(root.data+" ");
+        
+        printInorder(root.right);
+    }
+    
+    public static void main (String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t=Integer.parseInt(br.readLine());
+
+        while(t > 0){
+            String s = br.readLine();
+            Node root = buildTree(s);
+            
+            Solution ob = new Solution();
+            System.out.println(ob.maxPathSum(root));
+            t--;
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+/* class Node
+{
+    int data;
+    Node left, right;
+
+    Node(int item)
+    {
+        data = item;
+        left = right = null;
+    }
+} */
+class Solution
+{
+    int[] rec(Node root){
+        if(root==null){
+            return new int[]{-(int)(1e8),-(int)(1e8)};
+        }
+        
+        if(root.left==null && root.right==null){
+            return new int[]{root.data,-(int)(1e8)};
+        }
+        
+        int[] lans=rec(root.left);
+        int[] rans=rec(root.right);
+        
+        int lsum=lans[0];
+        int rsum=rans[0];
+        
+        int lmax=lans[1];
+        int rmax=rans[1];
+        
+        int sum=root.data + Math.max(lsum,rsum);
+        int max=Math.max(lmax,rmax);
+        
+        if(lsum!=-(int)(1e8) && rsum!=-(int)(1e8)){
+            max=Math.max(max,root.data+lsum+rsum);
+        }
+        
+        return new int[]{sum,max};
+    }
+    
+    int maxPathSum(Node root){
+        int[] ans=rec(root);
+        if(root.left==null) return Math.max(ans[0],ans[1]);
+        if(root.right==null) return Math.max(ans[0],ans[1]);
+        
+        return ans[1];
+    } 
+}
