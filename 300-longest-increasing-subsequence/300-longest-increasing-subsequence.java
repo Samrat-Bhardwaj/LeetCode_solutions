7980835cdfa805433922557d0cd6d0b43ac19aa1
Samrat Bhardwaj <68969450+Samrat-Bhardwaj@users.noreply.github.com>
2022-04-28 22:05:04 +0530

    Time: 4 ms (91.52%), Space: 41.6 MB (95.36%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
@@ -1,27 +1,37 @@
 class Solution {
+    public int BS(ArrayList<Integer> dp, int ele){
+        int si=0;
+        int ei=dp.size()-1;
+        
+        while(si<=ei){
+            int mid=(si+ei)/2;
+            
+            if(dp.get(mid)<ele){
+                si=mid+1;
+            } else {
+                ei=mid-1;
+            }
+        }
+        
+        return si;
+    }
     public int lengthOfLIS(int[] nums) {
         int n=nums.length;
         
-        int[] dp=new int[n];
-        
-        int lis=0;
+        ArrayList<Integer> dp=new ArrayList<>();
         
         for(int i=0; i<n; i++){
-            dp[i]=1;
+            int ele=nums[i];
             
-            for(int j=i-1; j>=0; j--){
-                if(nums[j]<nums[i]){
-                    dp[i]=Math.max(dp[i],dp[j]+1);
-                }
+            int pos=BS(dp,ele);
+            
+            if(pos==dp.size()){
+                dp.add(ele);
+            } else {
+                dp.set(pos,ele);
             }
-            
-            lis=Math.max(lis,dp[i]);
         }
         
-//         for(int i=0; i<n; i++){
-//             lis=Math.max(lis,dp[i]);
-//         }
-        
-        return lis;
+        return dp.size();
     }
 }
