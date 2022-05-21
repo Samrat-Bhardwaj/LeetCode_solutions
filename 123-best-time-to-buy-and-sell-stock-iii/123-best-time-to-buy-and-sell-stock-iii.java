632328a25bbcc737b2043ef950c274daad0da99f
Samrat Bhardwaj <68969450+Samrat-Bhardwaj@users.noreply.github.com>
2022-05-21 14:55:53 +0530

    Time: 6 ms (60.89%), Space: 75.6 MB (71.33%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.java
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.java
@@ -1,38 +1,19 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int K=2;
-        int n=prices.length;
+        int dpi10=0;
+        int dpi20=0;
+        int dpi11=-(int)(1e9);
+        int dpi21=-(int)(1e9);
         
-        int[][][] dp=new int[n][K+1][2];
-        
-        for(int i=0; i<n; i++){
-            for(int k=0; k<=K; k++){
-                for(int x=0; x<2; x++){
-                    if(k==0){
-                        if(x==0){
-                            dp[i][k][x]=0;
-                        } else {
-                            dp[i][k][x]=-(int)(1e9);
-                        }
-                    } else {
-                        if(i==0){
-                            if(x==0){
-                                dp[i][k][x]=Math.max(0,-(int)(1e9)+prices[i]);
-                            } else {
-                                dp[i][k][x]=Math.max(-(int)(1e9),0-prices[i]);
-                            }
-                        } else {
-                            if(x==0){
-                                dp[i][k][x]=Math.max(dp[i-1][k][0],dp[i-1][k][1]+prices[i]);
-                            } else {
-                                dp[i][k][x]=Math.max(dp[i-1][k][x],dp[i-1][k-1][0]-prices[i]);
-                            }
-                        }
-                    }
-                }
-            }
+        for(int i=0; i<prices.length; i++){
+            int p=prices[i];
+            
+            dpi20=Math.max(dpi20,dpi21+p);
+            dpi21=Math.max(dpi21,dpi10-p);
+            dpi10=Math.max(dpi10,dpi11+p);
+            dpi11=Math.max(dpi11,0-p);
         }
         
-        return dp[n-1][K][0];
+        return Math.max(dpi10,dpi20);
     }
 }
