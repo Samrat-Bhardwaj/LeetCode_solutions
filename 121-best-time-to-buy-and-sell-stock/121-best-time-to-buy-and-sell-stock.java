bc3aa23f0bd794e81667c036d59a73219ec22a4b
Samrat Bhardwaj <68969450+Samrat-Bhardwaj@users.noreply.github.com>
2022-05-21 11:05:55 +0530

    Time: 1 ms (100.00%), Space: 58.7 MB (93.41%) - LeetHub

---
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int dpi0=0;
+        int dpi1=-(int)(1e9);
+        
+        for(int i=0; i<prices.length; i++){
+            dpi0=Math.max(dpi0,dpi1+prices[i]);
+            dpi1=Math.max(dpi1,0-prices[i]);
+        }
+        
+        return dpi0;
+    }
+}
