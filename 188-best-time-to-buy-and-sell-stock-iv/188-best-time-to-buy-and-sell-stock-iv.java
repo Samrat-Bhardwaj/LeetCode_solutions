015c9f0d54679f1dc50e732ddc8bf29d3588092a
Samrat Bhardwaj <68969450+Samrat-Bhardwaj@users.noreply.github.com>
2022-05-21 14:07:59 +0530

    Time: 12 ms (29.75%), Space: 46.1 MB (14.11%) - LeetHub

---
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int maxProfit(int K, int[] prices) {
+        // int K=2;
+        int n=prices.length;
+        if(n==0) return 0;
+        
+        int[][][] dp=new int[n][K+1][2];
+        
+        for(int i=0; i<n; i++){
+            for(int k=0; k<=K; k++){
+                for(int x=0; x<2; x++){
+                    if(k==0){
+                        if(x==0){
+                            dp[i][k][x]=0;
+                        } else {
+                            dp[i][k][x]=-(int)(1e9);
+                        }
+                    } else {
+                        if(i==0){
+                            if(x==0){
+                                dp[i][k][x]=Math.max(0,-(int)(1e9)+prices[i]);
+                            } else {
+                                dp[i][k][x]=Math.max(-(int)(1e9),0-prices[i]);
+                            }
+                        } else {
+                            if(x==0){
+                                dp[i][k][x]=Math.max(dp[i-1][k][0],dp[i-1][k][1]+prices[i]);
+                            } else {
+                                dp[i][k][x]=Math.max(dp[i-1][k][x],dp[i-1][k-1][0]-prices[i]);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        
+        return dp[n-1][K][0];
+    }
+}
