eb24a14d2098b13b2cbbbaaab4689f7414314622
Samrat Bhardwaj <68969450+Samrat-Bhardwaj@users.noreply.github.com>
2022-09-16 19:32:47 +0530

    Time: 382 ms (23.76%), Space: 75.5 MB (85.52%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
@@ -1,66 +1,30 @@
 class Solution {
 public:
-vector<int> nsor(vector<int> arr, int n){
-    vector<int> nsr(n,0);
-
+int largestRectangleArea(vector<int>& heights) {
+    int area=0;
+    int n=heights.size();
     stack<int> st;
-
-    for(int i=n-1; i>=0; i--){
-        int ele=arr[i];
-
-        while(st.size()!=0 && arr[st.top()]>=ele){
-            st.pop();
-        }
-
-        if(st.size()==0){
-            nsr[i]=n;
-        } else {
-            nsr[i]=st.top();
-        }
-        
-        st.push(i);
-    }
-
-    return nsr;
-}
-    
-vector<int> nsol(vector<int> arr, int n){
-    vector<int> nsl(n,0);
-
-    stack<int> st;
+    st.push(-1);
 
     for(int i=0; i<n; i++){
-        int ele=arr[i];
-
-        while(st.size()!=0 && arr[st.top()]>=ele){
+        while(st.top()!=-1 && heights[st.top()]>=heights[i]){
+            int h=heights[st.top()];
             st.pop();
-        }
 
-        if(st.size()==0){
-            nsl[i]=-1;
-        } else {
-            nsl[i]=st.top();
+            int width = i - st.top() - 1;
+            area=max(area,h*width);
         }
-        
         st.push(i);
     }
 
-    return nsl;
-}
-    
-    int largestRectangleArea(vector<int>& heights) {
-        
-        vector<int> nsl = nsol(heights,heights.size());
-        vector<int> nsr = nsor(heights,heights.size());
-        
-        int maximum = -(int)1e9;
-        for(int i=0; i<heights.size(); i++){
-            int area = (nsr[i] - nsl[i] - 1)*heights[i];
-            maximum = max(maximum,area);
-            
-        }
-        
-        return maximum;
-        
+    while(st.top()!=-1){
+        int h=heights[st.top()];
+        st.pop();
+
+        int w=n-st.top()-1;
+        area=max(area,h*w);
     }
+
+    return area;
+}
 };
