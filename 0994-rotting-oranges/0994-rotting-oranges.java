c3512a517497fe23499b072f4adf31e789845224
Samrat Bhardwaj <68969450+Samrat-Bhardwaj@users.noreply.github.com>
2022-11-06 13:02:13 +0530

    Time: 2 ms (96.96%), Space: 41.6 MB (94.07%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,50 +1,49 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
         LinkedList<Integer> que=new LinkedList<>();
-        
-        int fresh_oranges=0;
         int n=grid.length;
         int m=grid[0].length;
-        
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                if(grid[i][j]==2){
+
+        int fresh_oranges=0;
+        for(int i=0; i<grid.length; i++){
+            for(int j=0; j<grid[0].length; j++){
+                if(grid[i][j]==1){
+                    fresh_oranges++;
+                } else if(grid[i][j]==2) {
                     int idx=i*m+j;
                     que.addLast(idx);
-                } else if(grid[i][j]==1){
-                    fresh_oranges++;
                 }
             }
         }
         
         if(fresh_oranges==0) return 0;
-        
-        int time=0;
-        int[][] dirs={{0,-1},{-1,0},{1,0},{0,1}};
-        
+
+        int level=0;
+        int[][] dirs={{0,1},{1,0},{0,-1},{-1,0}};
+
         while(que.size()>0){
             int size=que.size();
-            
+
             while(size-->0){
                 int idx=que.removeFirst();
-                
-                int x=idx/m;
-                int y=idx%m;
-                
+
+                int i=idx/m;
+                int j=idx%m;
+
                 for(int[] dir:dirs){
-                    int i=x+dir[0];
-                    int j=y+dir[1];
-                    
-                    if(i>=0 && j>=0 && i<n && j<m && grid[i][j]==1){
+                    int x=i+dir[0];
+                    int y=j+dir[1];
+
+                    if(x>=0 && y>=0 && x<n && y<m && grid[x][y]==1){
+                        que.addLast(x*m+y);
+                        grid[x][y]=2; // rotting oranges
                         fresh_oranges--;
-                        grid[i][j]=2;
-                        que.addLast(i*m+j);
                     }
                 }
             }
-            time++;
+            level++;
         }
-        
-        return fresh_oranges == 0 ? time - 1 : -1;
+
+        return fresh_oranges == 0 ? level - 1  : -1;
     }
 }
