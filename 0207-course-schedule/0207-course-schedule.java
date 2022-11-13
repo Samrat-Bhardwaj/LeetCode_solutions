ef14dfb5f6474e872d267bdbf39525b784acbda9
Samrat Bhardwaj <68969450+Samrat-Bhardwaj@users.noreply.github.com>
2022-11-13 12:20:51 +0530

    Time: 3 ms (97.73%), Space: 42.5 MB (95.24%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.java
@@ -0,0 +1,40 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        int[] indegree = new int[numCourses];
+        
+        ArrayList<Integer>[] graph = new ArrayList[numCourses];
+        LinkedList<Integer> que = new LinkedList<>();
+        ArrayList<Integer> ans = new ArrayList<>();
+        
+        for(int i=0; i<numCourses; i++){
+            graph[i] = new ArrayList<>();
+        }
+        
+        for(int[] pre:prerequisites){
+            int v = pre[0];
+            int u = pre[1];
+            
+            indegree[v]++;
+            graph[u].add(v);
+        }
+        
+        for(int i=0; i<numCourses; i++){
+            if(indegree[i]==0){
+                que.addLast(i);
+            }
+        }
+        
+        while(que.size()>0){
+            int u = que.removeFirst();
+            ans.add(u);
+
+            for(int e: graph[u]){
+                if(--indegree[e]==0){
+                    que.addLast(e);
+                }
+            }
+        }
+        
+        return ans.size() == numCourses;
+    }
+}
