d32744fb7ee5cec797de6994fcd1eea7295bb9b7
Samrat Bhardwaj <68969450+Samrat-Bhardwaj@users.noreply.github.com>
2022-12-15 23:14:30 +0530

    Added solution - LeetHub

---
+++ Stickler Thief - GFG/stickler-thief.java
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.io.*;
+
+class GFG
+ {
+	public static void main (String[] args)
+	 {
+	  
+	  //taking input using Scanner class
+	  Scanner sc = new Scanner(System.in);
+	  
+	  //taking count of testcases
+	  int t = sc.nextInt();
+	  while(t-- > 0){
+	      
+	      //taking count of houses
+	      int n = sc.nextInt();
+	      int arr[] = new int[n];
+	      
+	      //inserting money present in 
+	      //each house in the array
+	      for(int i = 0; i<n; ++i)
+	           arr[i] = sc.nextInt();
+  	      
+  	      //calling method FindMaxSum() of class solve
+  	      System.out.println(new Solution().FindMaxSum(arr, n));
+	 }
+   }
+}
+// } Driver Code Ends
+
+
+class Solution
+{
+    //Function to find the maximum money the thief can get.
+    public int findMax_memo(int idx, int[] arr, int[] memo){
+        if(idx == arr.length){
+            return memo[idx]=0;
+        }
+
+        if(memo[idx]!=0) return memo[idx];
+
+        int rob=arr[idx];
+        int notRob=0;
+
+        if(idx+2<=arr.length)
+            rob += findMax_memo(idx+2, arr,memo);
+        
+        notRob = findMax_memo(idx+1, arr,memo);
+
+        int ans = Math.max(rob,notRob);
+
+        return memo[idx]=ans;
+    }
+
+    public int FindMaxSum(int arr[], int n){
+        int[] memo= new int[n+1];
+        return findMax_memo(0, arr,memo);
+        // return findMax(0,arr);
+    }
+}
