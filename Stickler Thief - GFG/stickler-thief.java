060990d55a82c4b00fdc84aa310181d4017bf7eb
Samrat Bhardwaj <68969450+Samrat-Bhardwaj@users.noreply.github.com>
2022-12-15 23:17:03 +0530

    Added solution - LeetHub

--- Stickler Thief - GFG/stickler-thief.java
+++ Stickler Thief - GFG/stickler-thief.java
@@ -34,29 +34,33 @@ class GFG
 class Solution
 {
     //Function to find the maximum money the thief can get.
-    public int findMax_memo(int idx, int[] arr, int[] memo){
-        if(idx == arr.length){
-            return memo[idx]=0;
+    public int findMax_tabulation(int idx, int[] arr, int[] memo){
+        for(idx=arr.length; idx>=0; idx--){
+            if(idx == arr.length){
+                memo[idx]=0;
+                continue;
+            }
+    
+            int rob=arr[idx];
+            int notRob=0;
+    
+            if(idx+2<=arr.length)
+                rob += memo[idx+2];//findMax_memo(idx+2, arr,memo);
+            
+            notRob = memo[idx+1]; //findMax_memo(idx+1, arr,memo);
+    
+            int ans = Math.max(rob,notRob);
+    
+            memo[idx]=ans;
         }
 
-        if(memo[idx]!=0) return memo[idx];
-
-        int rob=arr[idx];
-        int notRob=0;
-
-        if(idx+2<=arr.length)
-            rob += findMax_memo(idx+2, arr,memo);
-        
-        notRob = findMax_memo(idx+1, arr,memo);
-
-        int ans = Math.max(rob,notRob);
-
-        return memo[idx]=ans;
+        return memo[0];
     }
 
     public int FindMaxSum(int arr[], int n){
         int[] memo= new int[n+1];
-        return findMax_memo(0, arr,memo);
+        return findMax_tabulation(0, arr, memo);
+        // return findMax_memo(0, arr,memo);
         // return findMax(0,arr);
     }
 }
