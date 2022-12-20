367566af8ecf41c836724b4f6b5b6b83635375ae
Samrat Bhardwaj <68969450+Samrat-Bhardwaj@users.noreply.github.com>
2022-12-20 22:53:20 +0530

    Added solution - LeetHub

---
+++ Minimum insertions to sort an array - GFG/minimum-insertions-to-sort-an-array.java
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+import java.io.*;
+import java.util.*;
+class GfG
+{
+    public static void main(String args[])
+        {
+            Scanner sc = new Scanner(System.in);
+            int t = sc.nextInt();
+            while(t-->0)
+                {
+                    int n = sc.nextInt();
+                    int Arr[] = new int[n];
+                    for(int i = 0;i<n;i++)
+                        Arr[i] = sc.nextInt();
+                    Solution ob = new Solution();
+                    System.out.println(ob.minInsertions(Arr,n));
+                }
+        }
+}    
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public int lengthOfLIS(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n];
+        int ans = 0;
+        for(int i=0; i<n; i++){
+            dp[i]=1;
+
+            for(int j=i-1; j>=0; j--){
+                if(nums[i] >= nums[j]){ // is addition of ith point possible after jth point 
+                    dp[i] = Math.max(dp[i],dp[j]+1);
+                }
+            }
+
+            ans = Math.max(ans,dp[i]);
+        }
+
+        return ans;
+    }
+    
+    public int minInsertions(int arr[], int N) { 
+        int lis = lengthOfLIS(arr);
+
+        return N - lis;
+	}
+}
